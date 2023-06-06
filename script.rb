ed5d84f86a1106a28bbb7c34a98729f1ef29bc94
Dion Scott <dionanthonyscott@gmail.com>
2023-06-06 00:51:15 -0500

    Function now able to handle multiple words in a string

--- script.rb
+++ script.rb
@@ -6,14 +6,13 @@ dictionary = ["below","down","go","going","horn","how","howdy",
 
 def substrings(string, dictionary)
     dictionary.reduce(Hash.new(0)) do |word, value| 
-        # p word
-        # p key
-        # p value
-        if string.include?(value)
+        downcase_string = string.downcase.split(" ")
+        simplified_string = downcase_string.join(" ")
+        if simplified_string.include?(value)
             word[value] += 1
         end
         word
     end
 end
 
-p substrings("below", dictionary)
+p substrings("Howdy, partner! Look it's down below!", dictionary)
